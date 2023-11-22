97a6e321955879459d48ba087d63640e8b13f7c7
Bhanu Pratap Sharma <84559130+bhanu2003-bh@users.noreply.github.com>
2023-11-22 11:41:29 +0530

    Create Bitstries.cpp

---
+++ Bitstries.cpp
@@ -0,0 +1,136 @@
+#define ll long long
+
+struct Node{
+
+Node* arr[3];
+bool flag  = false;
+
+bool contain(int x){
+    return arr[x]!=NULL;
+}
+void put(int x,Node* root){
+     arr[x] = root;
+}
+
+Node*get(int x){
+    return arr[x];
+}
+
+void setend(){
+  flag = true;
+}
+
+
+};
+
+class Trie {
+  private:  Node*root;
+public:
+    Trie() {
+        root = new Node();
+    }
+    
+    void insert(int x) {
+        Node*temp = root;
+        for(int i = 31;i>=0;i--){
+            int val = x & (1<<i);
+             if(val>0) val = 1;
+             else val = 0;
+
+
+            if(!temp->contain(val)){
+              temp->put(val,new Node());
+            }
+            temp = temp->get(val);
+        }
+        temp->setend();
+    }
+    
+    bool search(int x) {
+        Node*node = root;
+        for(int i = 31;i>=0;i--){
+            int val = x & (1<<i);
+             if(val>0) val = 1;
+             else val = 0;
+
+            if(!node->contain(val)){
+                return false;
+            }
+            node = node->get(val);
+        }
+        return node->flag;
+    }
+    
+    bool startsWith(int x) {
+         Node*node = root;
+       for(int i = 31;i>=0;i--){
+            int val = x & (1<<i);
+             if(val>0) val = 1;
+             else val = 0;
+
+
+            if(!node->contain(val)){
+                return false;
+            }
+            node = node->get(val);
+        }
+        return true;       
+    }
+
+
+    int maximumxor(int x){
+        Node*temp = root;
+        ll ans = 0;
+         for (int i = 31; i>=0; i--)
+         {
+            int val = x & (1<<i);
+             if(val>0) val = 1;
+             else val = 0;
+
+
+            if(temp->arr[1-val]!=NULL){
+                temp = temp->arr[1-val];
+                ans = ans+((1-val)*(1<<i));
+  
+            }
+            else {
+                ans = ans+((val)*(1<<i));
+                temp = temp->arr[val];
+        
+            }
+         }
+         return ans^x;
+    }
+};
+
+
+
+
+
+
+
+class Solution {
+public:
+int findMaximumXOR(vector<int>& nums) {
+
+int n = nums.size();
+
+
+Trie*n1 = new Trie();
+
+for (int i = 0; i <n; i++)
+{
+  n1->insert(nums[i]);
+}
+
+
+int maxi = 0;
+for (int i = 0; i <n; i++)
+{
+    maxi = max(maxi,n1->maximumxor(nums[i]));
+}
+
+return maxi;
+
+}
+};
